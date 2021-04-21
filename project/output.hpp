729348b7dba0971c86fb8caff753596a3ae75fcb
Yaniv Holder <yaniv.holder@outlook.com>
2021-04-21 17:19:02 +0100

    Fixed some files

--- project/output.hpp
+++ project/output.hpp
@@ -1,14 +1,13 @@
 #ifndef _236360_2_
 #define _236360_2_
 
+#include <string>
 using namespace std;
+
 namespace output {
-    extern const string rules[];
-
+	extern const string rules[];
     void printProductionRule(int ruleno);
-
     void errorLex(int lineno);
-
     void errorSyn(int lineno);
 };
 
