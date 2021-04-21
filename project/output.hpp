f0e7bc72dbf656a023152ecc34cce9b5646271dd
Yaniv Holder <yaniv.holder@outlook.com>
2021-04-21 16:46:51 +0100

    Write scanner.lex file

--- project/output.hpp
+++ project/output.hpp
@@ -1,1 +1,15 @@
-#ifndef _236360_2_ #define _236360_2_ #include using namespace std; namespace output { extern const string rules[]; void printProductionRule(int ruleno); void errorLex(int lineno); void errorSyn(int lineno); }; #endif 
+#ifndef _236360_2_
+#define _236360_2_
+
+using namespace std;
+namespace output {
+    extern const string rules[];
+
+    void printProductionRule(int ruleno);
+
+    void errorLex(int lineno);
+
+    void errorSyn(int lineno);
+};
+
+#endif
